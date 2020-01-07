861773804904b2d644f61e35a60d3f1bcf30e518
al-Maraqib <ahmed.m.almousawi@gmail.com>
2020-01-06 17:48:55 -0700

    Completed the beginning of the code, added the rest of the commands

--- DFO.py
+++ DFO.py
@@ -3,7 +3,7 @@ import os, glob, shutil
 dl = "D:\\Downloads"
 os.chdir(dl)
 dlfiles = glob.glob("D:\\Downloads\\*")
-folderslist = ['Executeables', 'Documents', 'Images', 'Torrents', 'Compressed', 'Misc.']
+folderslist = ['Executables', 'Documents', 'Images', 'Torrents', 'Compressed', 'Misc.']
 EXext = ['.exe', '.msi']
 DOCext = ['.docx', '.pptx', '.pdf']
 IMAGEext = ['.png', '.jpg', '.jpeg']
@@ -11,9 +11,9 @@ TORRENText = ['.torrent']
 COMPRESSEDext = ['.zip', '.rar', '.7z',]
 
 exeDetect = glob.glob('*.exe') + glob.glob('*.msi')
-docDetect = glob.glob('*.exe') + glob.glob('*.msi')
-imageDetect = glob.glob('*.exe') + glob.glob('*.msi')
-torrentDetect = glob.glob('*.exe') + glob.glob('*.msi')
+docDetect = glob.glob('*.docx') + glob.glob('*.pdf') + glob.glob('*.pptx') + glob.glob('*.xlsx') 
+imageDetect = glob.glob('*.png') + glob.glob('*.jpg') + glob.glob('*.jpeg')
+torrentDetect = glob.glob('*.torrent')
 compressedDetect = glob.glob('*.zip') + glob.glob('*.rar') + glob.glob('*.7z')
 
 for folder in folderslist:
@@ -24,5 +24,21 @@ print("Current files and Directories in '% s':" % dl)
 print(dlfiles)
 
 for files in exeDetect:
-    print(files)
-    shutil.move(files,(os.path.join(dl,"Executeables")))
+    print("An executable was detected: %s" %files)
+    shutil.move(files,(os.path.join(dl,"Executables")))
+
+for files in docDetect:
+    print("A document was detected: %s" %files)
+    shutil.move(files,(os.path.join(dl,"Documents")))
+
+for files in imageDetect:
+    print("An image was detected: %s" %files)
+    shutil.move(files,(os.path.join(dl,"Images")))
+
+for files in torrentDetect:
+        print("A torrent was detected: %s" %files)
+        shutil.move(files,(os.path.join(dl,"Torrents")))
+
+for files in compressedDetect:
+    print("A compressed file was detected: %s" %files)
+    shutil.move(files,(os.path.join(dl,"Compressed")))
